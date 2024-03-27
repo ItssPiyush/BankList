c94f2000adeab147931354a6da25a3f19ee5a236
ItssPiyush <itsspiyush@gmail.com>
2024-03-27 19:51:27 +0530

    implemented close account funcation

--- script.js
+++ script.js
@@ -161,6 +161,22 @@ btnTransfer.addEventListener('click' , function(e){
     updateUi(currentAccount);
   }
 
+});
+
+btnClose.addEventListener('click' , function(e){
+  e.preventDefault();
+  if(inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin)
+  {
+    const index= accounts.findIndex(acc => acc.username === currentAccount.username);
+    // delete account
+    accounts.splice(index,1);
+    
+    // hide Ui
+    containerApp.style.opacity = 0;
+    inputCloseUsername.value = inputClosePin.value = '';
+    inputClosePin.blur();
+    
+  }
 })
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
