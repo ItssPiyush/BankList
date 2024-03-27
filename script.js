9c504141634cf2823e5c37b2093244573937dd0a
ItssPiyush <itsspiyush@gmail.com>
2024-03-27 15:37:40 +0530

    fixed Transfer

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ btnTransfer.addEventListener('click' , function(e){
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
   const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
-  inputTransferAmount.value = inputTransferTo = '';
+  inputTransferAmount.value = inputTransferTo.value = '';
   if(amount > 0 && receiverAcc && currentAccount.balance >= amount && receiverAcc?.username !== currentAccount.username){
 
     //doing the tansfer
