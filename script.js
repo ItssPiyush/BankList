d6cd053dad71298986edaba14c83290e7fa2c088
ItssPiyush <itsspiyush@gmail.com>
2024-03-30 18:51:59 +0530

    added the short function and few more thing

--- script.js
+++ script.js
@@ -61,14 +61,16 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function(mov , i){
+
+  const movs = sort ? movements.slice().sort((a,b) => a - b) : movements
+  movs.forEach(function(mov , i){
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
           <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-          <div class="movements__value">${mov}₹</div>
+          <div class="movements__value">${mov.toFixed(2)}₹</div>
         </div>
         `;
 
@@ -90,7 +92,7 @@ console.log(accounts)
 
 const calcDisplayBalance = function(acc){
   acc.balance = acc.movements.reduce((acc,curr) => acc += curr ,0);
-  labelBalance.textContent = `${acc.balance}₹`
+  labelBalance.textContent = `${acc.balance.toFixed(2)}₹`
 }
 
 
@@ -99,12 +101,12 @@ const calcDisplaySummary = function(acc){
   const incomes = acc.movements
   .filter(mov => mov > 0)
   . reduce((acc,mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}₹`
+  labelSumIn.textContent = `${incomes.toFixed(2)}₹`
 
   const out = acc.movements
   .filter(mov => mov < 0)
   .reduce((acc,mov)=> acc + mov , 0);
-  labelSumOut.textContent = `${Math.abs(out)}₹`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}₹`;
 
   const interest = acc.movements
   .filter(mov => mov >0)
@@ -113,7 +115,7 @@ const calcDisplaySummary = function(acc){
     return int >= 1;
   })
   .reduce((acc,mov) => acc + mov, 0);
-  labelSumInterest.textContent = `${interest}₹`
+  labelSumInterest.textContent = `${interest.toFixed(2)}₹`
 
 }
 const updateUi = function(acc){
@@ -165,7 +167,7 @@ btnTransfer.addEventListener('click' , function(e){
 
 btnLoan.addEventListener('click',function(e){
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
   if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)){
     // Add movement 
     currentAccount.movements.push(amount);
@@ -191,6 +193,13 @@ btnClose.addEventListener('click' , function(e){
     
   }
 })
+
+let sorted = false;
+btnSort.addEventListener('click', function(e){
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+})
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -204,6 +213,10 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 
+// const z = Array.from({length:7},(_,i) => i+1);
+// console.log(z)
+// const accMovements = accounts.map;
+// console.log(accMovements);
 // const eurToUsd = 1.1;
 // const movementsUSD = movements.map(mov => 
 //     mov * eurToUsd);
